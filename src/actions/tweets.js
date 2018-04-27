f600f4587cd58c39226fbc83d45b518f6ed85534
Kosta <k.v.dinkov@gmail.com>
2018-04-27 12:42:12 +0300

    add: new tweet component creation; tweet details page;

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,30 +1,55 @@
-import { saveLikeToggle} from '../utils/api';
+import {saveLikeToggle, saveTweet} from '../utils/api';
+import {showLoading, hideLoading} from 'react-redux-loading';
 
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS';
 export const TOGGLE_TWEET = 'TOGGLE_TWEET';
+export const ADD_TWEET = 'ADD_TWEET';
 
-export function receiveTweets(tweets){
-  return {type: RECEIVE_TWEETS, tweets}
+export function receiveTweets(tweets) {
+  return { type: RECEIVE_TWEETS, tweets };
 }
 
-function toggleTweet({id, authedUser, hasLiked}){
+function toggleTweet({ id, authedUser, hasLiked }) {
   return {
     type: TOGGLE_TWEET,
     id,
     authedUser,
     hasLiked
-  }
+  };
 }
 
-export function handleToggleTweet(info){
-  return (dispatch)=>{
+export function handleToggleTweet(info) {
+  return (dispatch) => {
     dispatch(toggleTweet(info));
 
     return saveLikeToggle(info)
-      .catch((e)=>{
-        console.warn('Error in handleToggleTweet',e);
+      .catch((e) => {
+        console.warn('Error in handleToggleTweet', e);
         dispatch(toggleTweet(info));
-        alert('There was an error liking the tweet. Try again.')
-      })
-  }
+        alert('There was an error liking the tweet. Try again.');
+      });
+  };
+}
+
+function addTweet(tweet) {
+  return {
+    type: ADD_TWEET,
+    tweet
+  };
+}
+
+export function handleAddTweet(text, replyingTo) {
+  return (dispatch, getState) => {
+
+    const { authedUser } = getState();
+
+    dispatch(showLoading());
+    saveTweet({
+      text,
+      author: authedUser,
+      replyingTo
+    })
+      .then((tweet) => dispatch(addTweet(tweet)))
+      .then(() => dispatch(hideLoading()));
+  };
 }
